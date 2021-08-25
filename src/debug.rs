6ccfdf1c774923d252178fb84b56e03ea0cf6a3c
Luis Wirth <lwirth2000@gmail.com>
2021-08-25 18:47:50 +0200

    pico explorer: text, set_pixels
    - set up displaying text
    - custom panic handler for displaying text on screen
    - set_pixels with iterators
    - [u16; SIZE2] too big for storing screen contents

---
+++ src/debug.rs
@@ -0,0 +1,107 @@
+use core::{fmt::Write, panic::PanicInfo};
+
+use embedded_graphics::{
+    mono_font::{MonoFont, MonoTextStyleBuilder},
+    pixelcolor::Rgb565,
+    prelude::*,
+    text::{Alignment, Baseline, LineHeight, Text, TextStyleBuilder},
+};
+use embedded_hal::digital::v2::OutputPin;
+use pico_explorer::{
+    hal::gpio::{bank0::Gpio25, Output, Pin, PushPull},
+    Screen,
+};
+
+pub type Led = Pin<Gpio25, Output<PushPull>>;
+pub type ArrayString = arrayvec::ArrayString<{ NCHARS as usize }>;
+
+pub const SCREEN_SIZE: u16 = 240;
+pub const SCREEN_SIZE2: usize = SCREEN_SIZE as usize * SCREEN_SIZE as usize;
+
+static mut SCREEN: Option<Screen> = None;
+static mut LED: Option<Led> = None;
+
+pub fn led<'a>() -> &'a Led {
+    unsafe { LED.as_ref().unwrap() }
+}
+
+pub fn led_mut<'a>() -> &'a mut Led {
+    unsafe { LED.as_mut().unwrap() }
+}
+
+pub fn screen<'a>() -> &'a Screen {
+    unsafe { SCREEN.as_ref().unwrap() }
+}
+pub fn screen_mut<'a>() -> &'a mut Screen {
+    unsafe { SCREEN.as_mut().unwrap() }
+}
+
+pub fn init_debug(led: Led, screen: Screen) {
+    unsafe {
+        LED = Some(led);
+    }
+    unsafe {
+        SCREEN = Some(screen);
+    }
+}
+
+const FONT: MonoFont = embedded_graphics::mono_font::ascii::FONT_4X6;
+
+const CHAR_WIDTH: u32 = FONT.character_size.width + FONT.character_spacing;
+const CHAR_HEIGHT: u32 = FONT.character_size.height;
+const CHARS_PER_ROW: u32 = SCREEN_SIZE as u32 / CHAR_WIDTH;
+const CHARS_PER_COL: u32 = SCREEN_SIZE as u32 / CHAR_HEIGHT;
+const NCHARS: u32 = CHARS_PER_ROW * CHARS_PER_COL;
+
+pub fn breakup(s: ArrayString) -> ArrayString {
+    let mut r = ArrayString::new();
+    let mut last = 0usize;
+    for (i, c) in s.char_indices() {
+        r.push(c);
+        if c == '\n' {
+            last = i;
+        } else if i - last >= CHARS_PER_ROW as usize {
+            r.push('\n');
+            last = i;
+        }
+    }
+    r
+}
+
+pub fn sprint(text: &str) {
+    screen_mut().clear(Rgb565::BLACK).unwrap();
+    draw_text(text);
+}
+
+pub fn draw_text(text: &str) {
+    let char_style = MonoTextStyleBuilder::new()
+        .font(&FONT)
+        .text_color(Rgb565::GREEN)
+        .background_color(Rgb565::BLACK)
+        //.reset_background_color()
+        .build();
+
+    let text_style = TextStyleBuilder::new()
+        .alignment(Alignment::Left)
+        .baseline(Baseline::Top)
+        .line_height(LineHeight::Pixels(6))
+        .build();
+
+    Text::with_text_style(text, Point::new(0, 0), char_style, text_style)
+        .draw(screen_mut())
+        .unwrap();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    led_mut().set_high().unwrap();
+
+    let mut buf = ArrayString::new();
+    writeln!(buf, "{}", info).unwrap();
+    buf = breakup(buf);
+
+    screen_mut().clear(Rgb565::RED).unwrap();
+    draw_text(&buf);
+
+    loop {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,22 @@
 #![no_std]
 #![no_main]
 
-use arrayvec::ArrayString;
-use core::fmt::Write;
+mod debug;
+
+use debug::SCREEN_SIZE2;
+
 use cortex_m_rt::entry;
-use embedded_graphics::{
-    mono_font::{ascii::FONT_10X20, MonoTextStyleBuilder},
-    pixelcolor::Rgb565,
-    prelude::*,
-    text::{Alignment, Text},
+use embedded_time::fixed_point::FixedPoint;
+use pico_explorer::{
+    hal::{self, adc::Adc, clocks::ClockSource, sio::Sio, watchdog::Watchdog},
+    pac, PicoExplorer, XOSC_CRYSTAL_FREQ,
 };
-use embedded_hal::digital::v2::OutputPin;
-use embedded_time::rate::*;
-use hal::{adc::Adc, clocks::*, sio::Sio, watchdog::Watchdog};
-use panic_semihosting as _;
-use pico_explorer::{hal, pac, Button, PicoExplorer, XOSC_CRYSTAL_FREQ};
+
+use crate::debug::SCREEN_SIZE;
 
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER;
-
-// See 4.9.5 from RP2040 datasheet
-fn calc_temp(adc_value: f32, refv: f64) -> f64 {
-    let vbe: f64 = f64::from(adc_value) * refv;
-    27f64 - (vbe - 0.706) / 0.001721
-}
 
 #[entry]
 fn main() -> ! {
@@ -33,7 +25,7 @@ fn main() -> ! {
 
     // Enable watchdog and clocks
     let mut watchdog = Watchdog::new(p.WATCHDOG);
-    let clocks = init_clocks_and_plls(
+    let clocks = hal::clocks::init_clocks_and_plls(
         XOSC_CRYSTAL_FREQ,
         p.XOSC,
         p.CLOCKS,
@@ -47,13 +39,10 @@ fn main() -> ! {
 
     let mut delay = cortex_m::delay::Delay::new(cp.SYST, clocks.system_clock.get_freq().integer());
 
-    // Enable adc
-    let mut adc = Adc::new(p.ADC, &mut p.RESETS);
-    let mut temp_sense = adc.enable_temp_sensor();
-
+    let adc = Adc::new(p.ADC, &mut p.RESETS);
     let sio = Sio::new(p.SIO);
 
-    let (mut explorer, pins) = PicoExplorer::new(
+    let (explorer, pins) = PicoExplorer::new(
         p.IO_BANK0,
         p.PADS_BANK0,
         sio.gpio_bank0,
@@ -63,49 +52,22 @@ fn main() -> ! {
         &mut delay,
     );
 
-    let mut led = pins.led.into_push_pull_output();
+    let led = pins.led.into_push_pull_output();
 
-    let mut even = true;
-    loop {
-        delay.delay_ms(500);
+    debug::init_debug(led, explorer.screen);
 
-        // Set GPIO25 to be low
-        led.set_low().unwrap();
+    let mut text = debug::ArrayString::new();
+    text.push_str(include_str!("../res/faust.txt"));
+    let text = debug::breakup(text);
 
-        delay.delay_ms(500);
-
-        // Set GPIO25 to be high
-        led.set_high().unwrap();
-
-        let adc_value = explorer.get_adc(&mut temp_sense);
-        let temp: f64 = calc_temp(adc_value, 3.3);
-
-        // Create a fixed buffer to store screen contents
-        let mut buf = ArrayString::<100>::new();
-
-        // Write to buffer
-        writeln!(&mut buf, "Hello World {}", if even { '|' } else { '-' }).unwrap();
-        writeln!(&mut buf, "Temp: {:.1}", temp).unwrap();
-        writeln!(
-            &mut buf,
-            "A:{:.1} B:{:.1}\nX:{:.1} Y:{:.1}",
-            explorer.is_pressed(Button::A),
-            explorer.is_pressed(Button::B),
-            explorer.is_pressed(Button::X),
-            explorer.is_pressed(Button::Y)
-        )
+    let pixels = 0..SCREEN_SIZE2 as u16;
+    debug::screen_mut()
+        .set_pixels(0, 0, SCREEN_SIZE - 1, SCREEN_SIZE - 1, pixels)
         .unwrap();
 
-        // Draw buffer on screen
-        let style = MonoTextStyleBuilder::new()
-            .font(&FONT_10X20)
-            .text_color(Rgb565::GREEN)
-            .background_color(Rgb565::BLACK)
-            .build();
-        Text::with_alignment(&buf, Point::new(20, 30), style, Alignment::Left)
-            .draw(&mut explorer.screen)
-            .unwrap();
+    debug::draw_text(&text);
 
-        even = !even;
+    loop {
+        cortex_m::asm::nop();
     }
 }
