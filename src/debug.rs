57f6120ab006b27fcdeaafe569b4cfc8b17a22be
Luis Wirth <lwirth2000@gmail.com>
2021-09-09 13:28:30 +0200

    first attempt at lights api; light show example

--- src/debug.rs
+++ src/debug.rs
@@ -1,15 +1,15 @@
 use core::{fmt::Write, panic::PanicInfo};
 
 use embedded_graphics::{
-    mono_font::{MonoFont, MonoTextStyleBuilder},
-    pixelcolor::Rgb565,
-    prelude::*,
-    text::{Alignment, Baseline, LineHeight, Text, TextStyleBuilder},
+  mono_font::{MonoFont, MonoTextStyleBuilder},
+  pixelcolor::Rgb565,
+  prelude::*,
+  text::{Alignment, Baseline, LineHeight, Text, TextStyleBuilder},
 };
 use embedded_hal::digital::v2::OutputPin;
 use pico_explorer::{
-    hal::gpio::{bank0::Gpio25, Output, Pin, PushPull},
-    Screen,
+  hal::gpio::{bank0::Gpio25, Output, Pin, PushPull},
+  Screen,
 };
 
 pub type Led = Pin<Gpio25, Output<PushPull>>;
@@ -21,28 +21,38 @@ pub const SCREEN_SIZE2: usize = SCREEN_SIZE as usize * SCREEN_SIZE as usize;
 static mut SCREEN: Option<Screen> = None;
 static mut LED: Option<Led> = None;
 
+fn usage() {
+  //let led = pins.led.into_push_pull_output();
+
+  //debug::init_debug(led, explorer.screen);
+  //let mut string = debug::ArrayString::new();
+
+  //let string = debug::breakup(string);
+  //debug::sprint(&string);
+}
+
 pub fn led<'a>() -> &'a Led {
-    unsafe { LED.as_ref().unwrap() }
+  unsafe { LED.as_ref().unwrap() }
 }
 
 pub fn led_mut<'a>() -> &'a mut Led {
-    unsafe { LED.as_mut().unwrap() }
+  unsafe { LED.as_mut().unwrap() }
 }
 
 pub fn screen<'a>() -> &'a Screen {
-    unsafe { SCREEN.as_ref().unwrap() }
+  unsafe { SCREEN.as_ref().unwrap() }
 }
 pub fn screen_mut<'a>() -> &'a mut Screen {
-    unsafe { SCREEN.as_mut().unwrap() }
+  unsafe { SCREEN.as_mut().unwrap() }
 }
 
 pub fn init_debug(led: Led, screen: Screen) {
-    unsafe {
-        LED = Some(led);
-    }
-    unsafe {
-        SCREEN = Some(screen);
-    }
+  unsafe {
+    LED = Some(led);
+  }
+  unsafe {
+    SCREEN = Some(screen);
+  }
 }
 
 const FONT: MonoFont = embedded_graphics::mono_font::ascii::FONT_4X6;
@@ -54,54 +64,54 @@ const CHARS_PER_COL: u32 = SCREEN_SIZE as u32 / CHAR_HEIGHT;
 const NCHARS: u32 = CHARS_PER_ROW * CHARS_PER_COL;
 
 pub fn breakup(s: ArrayString) -> ArrayString {
-    let mut r = ArrayString::new();
-    let mut last = 0usize;
-    for (i, c) in s.char_indices() {
-        r.push(c);
-        if c == '\n' {
-            last = i;
-        } else if i - last >= CHARS_PER_ROW as usize {
-            r.push('\n');
-            last = i;
-        }
+  let mut r = ArrayString::new();
+  let mut last = 0usize;
+  for (i, c) in s.char_indices() {
+    r.push(c);
+    if c == '\n' {
+      last = i;
+    } else if i - last >= CHARS_PER_ROW as usize {
+      r.push('\n');
+      last = i;
     }
-    r
+  }
+  r
 }
 
 pub fn sprint(text: &str) {
-    screen_mut().clear(Rgb565::BLACK).unwrap();
-    draw_text(text);
+  screen_mut().clear(Rgb565::BLACK).unwrap();
+  draw_text(text);
 }
 
 pub fn draw_text(text: &str) {
-    let char_style = MonoTextStyleBuilder::new()
-        .font(&FONT)
-        .text_color(Rgb565::GREEN)
-        .background_color(Rgb565::BLACK)
-        //.reset_background_color()
-        .build();
+  let char_style = MonoTextStyleBuilder::new()
+    .font(&FONT)
+    .text_color(Rgb565::GREEN)
+    .background_color(Rgb565::BLACK)
+    //.reset_background_color()
+    .build();
 
-    let text_style = TextStyleBuilder::new()
-        .alignment(Alignment::Left)
-        .baseline(Baseline::Top)
-        .line_height(LineHeight::Pixels(6))
-        .build();
+  let text_style = TextStyleBuilder::new()
+    .alignment(Alignment::Left)
+    .baseline(Baseline::Top)
+    .line_height(LineHeight::Pixels(6))
+    .build();
 
-    Text::with_text_style(text, Point::new(0, 0), char_style, text_style)
-        .draw(screen_mut())
-        .unwrap();
+  Text::with_text_style(text, Point::new(0, 0), char_style, text_style)
+    .draw(screen_mut())
+    .unwrap();
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    led_mut().set_high().unwrap();
+  led_mut().set_high().unwrap();
 
-    let mut buf = ArrayString::new();
-    writeln!(buf, "{}", info).unwrap();
-    buf = breakup(buf);
+  let mut buf = ArrayString::new();
+  writeln!(buf, "{}", info).unwrap();
+  buf = breakup(buf);
 
-    screen_mut().clear(Rgb565::RED).unwrap();
-    draw_text(&buf);
+  screen_mut().clear(Rgb565::RED).unwrap();
+  draw_text(&buf);
 
-    loop {}
+  loop {}
 }
--- src/main.rs
+++ src/main.rs
@@ -2,164 +2,122 @@
 #![no_main]
 
 mod debug;
+mod lights;
 
-use core::fmt::Write;
 use cortex_m::delay::Delay;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
+
 use embedded_time::fixed_point::FixedPoint;
+use lights::Lights;
+
 use pico_explorer::{
-    hal::{
-        self, adc::Adc, clocks::ClockSource, sio::Sio, uart::UartPeripheral, watchdog::Watchdog,
-    },
-    pac, PicoExplorer, XOSC_CRYSTAL_FREQ,
+  hal::{self, adc::Adc, clocks::ClockSource, sio::Sio, uart::UartPeripheral, watchdog::Watchdog},
+  pac, PicoExplorer, XOSC_CRYSTAL_FREQ,
 };
 
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER;
 
-#[entry]
-fn main() -> ! {
+//static SHARED: Mutex<RefCell<Option<Shared>>> = Mutex::default();
+//pub struct Shared {
+//  pub delay: Delay,
+//}
+
+struct App {
+  //p: pac::Peripherals,
+  //cp: pac::CorePeripherals,
+  //clocks: ClocksManager,
+  //adc: Adc,
+  //sio: Sio,
+  //pins: Pins,
+  _explorer: PicoExplorer,
+  lights: Lights,
+  _uart: UartPeripheral<hal::uart::Enabled, pac::UART0>,
+}
+
+impl App {
+  fn init() -> Self {
     let mut p = pac::Peripherals::take().unwrap();
     let cp = pac::CorePeripherals::take().unwrap();
 
-    // Enable watchdog and clocks
     let mut watchdog = Watchdog::new(p.WATCHDOG);
     let clocks = hal::clocks::init_clocks_and_plls(
-        XOSC_CRYSTAL_FREQ,
-        p.XOSC,
-        p.CLOCKS,
-        p.PLL_SYS,
-        p.PLL_USB,
-        &mut p.RESETS,
-        &mut watchdog,
+      XOSC_CRYSTAL_FREQ,
+      p.XOSC,
+      p.CLOCKS,
+      p.PLL_SYS,
+      p.PLL_USB,
+      &mut p.RESETS,
+      &mut watchdog,
     )
     .ok()
     .unwrap();
     let mut delay = Delay::new(cp.SYST, clocks.system_clock.get_freq().integer());
+
     let adc = Adc::new(p.ADC, &mut p.RESETS);
     let sio = Sio::new(p.SIO);
 
     let (explorer, pins) = PicoExplorer::new(
-        p.IO_BANK0,
-        p.PADS_BANK0,
-        sio.gpio_bank0,
-        p.SPI0,
-        adc,
-        &mut p.RESETS,
-        &mut delay,
+      p.IO_BANK0,
+      p.PADS_BANK0,
+      sio.gpio_bank0,
+      p.SPI0,
+      adc,
+      &mut p.RESETS,
+      &mut delay,
     );
-    //let _pin0: hal::gpio::Pin<_, hal::gpio::FunctionPio0> = pins.gpio0.into_push_pull_output().into_mode();
-    let _pin2: hal::gpio::Pin<_, hal::gpio::FunctionPio0> = pins.gpio2.into_mode();
 
-    let led = pins.led.into_push_pull_output();
-
-    debug::init_debug(led, explorer.screen);
-    let mut string = debug::ArrayString::new();
-
-    let side_set = pio::SideSet::new(true, 1, false);
-
-    let mut assembler = pio::Assembler::new_with_side_set(side_set);
-
-    // configure pin as output
-    // TODO: is this really necessary for side_set?
-    assembler.set(pio::SetDestination::PINDIRS, 1);
-
-    let mut wrap_target = assembler.label();
-    let mut bitloop = assembler.label();
-    let mut do_one = assembler.label();
-    let mut do_zero = assembler.label();
-    let mut wrap_source = assembler.label();
-
-    const T1: u8 = 2;
-    const T2: u8 = 5;
-    const T3: u8 = 3;
-
-    assembler.bind(&mut wrap_target);
-    assembler.bind(&mut bitloop);
-    assembler.out_with_delay_and_side_set(pio::OutDestination::X, 1, T3 - 1, 0);
-    assembler.jmp_with_delay_and_side_set(pio::JmpCondition::XIsZero, &mut do_zero, T1 - 1, 1);
-    assembler.bind(&mut do_one);
-    assembler.jmp_with_delay_and_side_set(pio::JmpCondition::Always, &mut bitloop, T2 - 1, 1);
-    assembler.bind(&mut do_zero);
-    // Pseudoinstruction: NOP
-    assembler.mov_with_delay_and_side_set(
-        pio::MovDestination::Y,
-        pio::MovOperation::None,
-        pio::MovSource::Y,
-        T2 - 1,
-        0,
+    let lights = Lights::init(
+      p.PIO0,
+      pins.gpio2.into_mode(),
+      &mut p.RESETS,
+      clocks.system_clock.get_freq().0 as f32,
+      delay,
     );
-    assembler.bind(&mut wrap_source);
 
-    let program = assembler.assemble(Some((wrap_source, wrap_target)));
+    let _uart_tx_pin = pins.gpio0.into_mode::<hal::gpio::FunctionUart>();
+    let _uart_rx_pin = pins.gpio1.into_mode::<hal::gpio::FunctionUart>();
 
-    let pio = hal::pio::PIO::new(p.PIO0, &mut p.RESETS);
-    let sm = &pio.state_machines()[0];
-
-    let clock_freq = clocks.system_clock.get_freq().0 as f32;
-    const FREQ: f32 = 8_000_000.0;
-    let div = clock_freq / FREQ;
-
-    let builder = hal::pio::PIOBuilder::default()
-        .with_program(&program)
-        .buffers(hal::pio::Buffers::OnlyTx)
-        .out_shift_direction(hal::pio::ShiftDirection::Left)
-        .autopull(true)
-        .pull_threshold(32)
-        .clock_divisor(div)
-        .set_pins(2, 1)
-        .side_set(side_set)
-        .side_set_pin_base(2);
-
-    builder.build(&pio, sm).unwrap();
-
-    sm.set_enabled(true);
-
-    const NLED: usize = 60;
-
-    let string = debug::breakup(string);
-    debug::sprint(&string);
-
-    let uart = UartPeripheral::<_, _>::enable(
-        p.UART0,
-        &mut p.RESETS,
-        hal::uart::common_configs::_115200_8_N_1,
-        clocks.peripheral_clock.into(),
+    let uart = UartPeripheral::enable(
+      p.UART0,
+      &mut p.RESETS,
+      hal::uart::common_configs::_115200_8_N_1,
+      clocks.peripheral_clock.into(),
     )
     .unwrap();
 
-    let _tx_pin = pins.gpio0.into_mode::<hal::gpio::FunctionUart>();
-    let _rx_pin = pins.gpio1.into_mode::<hal::gpio::FunctionUart>();
+    //let shared = Shared { delay };
+    //interrupt::free(|cs| SHARED.borrow(cs).replace(Some(shared)));
 
-    uart.write_full_blocking(b"UART Test!!!\r\n");
-    hprintln!("semihosting test!!").unwrap();
-
-    let push = |c| {
-        while !sm.write_tx(c) {}
-    };
-
-    loop {
-        for _ in 0..20 {
-            push(0xFF_00_00_00);
-        }
-        for _ in 20..40 {
-            push(0x00_FF_00_00);
-        }
-        for _ in 40..60 {
-            push(0x00_00_FF_00);
-        }
-        delay.delay_ms(1000);
-        //cortex_m::asm::nop();
+    Self {
+      //p,
+      //cp,
+      //clocks,
+      //adc,
+      //sio,
+      _explorer: explorer,
+      //pins,
+      _uart: uart,
+      lights,
     }
+  }
+
+  /// Get a reference to the app's lights.
+  fn lights_mut(&mut self) -> &mut Lights {
+    &mut self.lights
+  }
 }
 
-fn color([r, g, b, w]: [u8; 4]) -> u32 {
-    let mut grbw = 0u32;
-    grbw |= (g as u32) << 24;
-    grbw |= (r as u32) << 16;
-    grbw |= (b as u32) << 8;
-    grbw |= w as u32;
-    grbw
+#[entry]
+fn main() -> ! {
+  let mut app = App::init();
+
+  app
+    .lights_mut()
+    .play_lightshow(&mut lights::ExampleLightshow {});
+
+  loop {
+    cortex_m::asm::nop();
+  }
 }
