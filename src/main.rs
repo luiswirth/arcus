e20988575135583d1af1e517ad021cf5c2a09508
Luis Wirth <lwirth2000@gmail.com>
2022-02-03 15:35:04 +0100

    initial rtic test

--- src/main.rs
+++ src/main.rs
@@ -1,130 +1,85 @@
 #![no_std]
 #![no_main]
-#![feature(default_alloc_error_handler)]
-
-#[allow(unused_imports)]
-#[macro_use]
-extern crate alloc;
+#![deny(unsafe_code)]
 
 extern crate panic_semihosting;
-
-pub mod light;
-
-use cortex_m::delay::Delay;
-use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
-
-use alloc_cortex_m::CortexMHeap;
-use alloc::{boxed::Box, vec::Vec};
-
-use rp_pico::{
-  hal::{self, adc::Adc, clocks::ClockSource, sio::Sio, uart::UartPeripheral, watchdog::Watchdog},
-  pac, XOSC_CRYSTAL_FREQ, Pins,
-};
-
-use embedded_time::fixed_point::FixedPoint;
-
-use light::{
-  color::Color,
-  show::{self, Show, UniformShow},
-  Lights, Utils,
-};
 
 #[link_section = ".boot_loader"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
-#[global_allocator]
-static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+// dispatchers?
+#[rtic::app(device = rp_pico::hal::pac, peripherals = true)]
+mod app {
+  use embedded_hal::digital::v2::OutputPin;
+  use embedded_time::duration::Extensions;
+  use rp_pico::hal::{self, clocks, gpio, Sio};
 
-struct App {
-  lights: Lights,
-  utils: Utils,
+  const SCAN_TIME_US: u32 = 1_000_000;
 
-  stack: Vec<Box<dyn Show>>,
-}
+  #[shared]
+  struct Shared {
+    timer: hal::Timer,
+    alarm: hal::timer::Alarm0,
+    led: gpio::Pin<gpio::pin::bank0::Gpio25, gpio::PushPullOutput>,
+  }
 
-impl App {
-  fn init() -> Self {
-    // Setup global allocator with conservative heap.
-    {
-      let start = cortex_m_rt::heap_start() as usize;
-      let size = 200 * 1024;
-      unsafe { ALLOCATOR.init(start, size) }
-    }
+  #[local]
+  struct Local {}
 
-    let mut p = pac::Peripherals::take().unwrap();
-    let cp = pac::CorePeripherals::take().unwrap();
-
-    let mut watchdog = Watchdog::new(p.WATCHDOG);
-    let clocks = hal::clocks::init_clocks_and_plls(
-      XOSC_CRYSTAL_FREQ,
-      p.XOSC,
-      p.CLOCKS,
-      p.PLL_SYS,
-      p.PLL_USB,
-      &mut p.RESETS,
+  #[init]
+  fn init(c: init::Context) -> (Shared, Local, init::Monotonics) {
+    let mut resets = c.device.RESETS;
+    let mut watchdog = hal::Watchdog::new(c.device.WATCHDOG);
+    let _clocks = clocks::init_clocks_and_plls(
+      rp_pico::XOSC_CRYSTAL_FREQ,
+      c.device.XOSC,
+      c.device.CLOCKS,
+      c.device.PLL_SYS,
+      c.device.PLL_USB,
+      &mut resets,
       &mut watchdog,
     )
     .ok()
     .unwrap();
-    let delay = Delay::new(cp.SYST, clocks.system_clock.get_freq().integer());
 
-    let _adc = Adc::new(p.ADC, &mut p.RESETS);
-    let sio = Sio::new(p.SIO);
-
-    let pins = Pins::new(
-      p.IO_BANK0,
-      p.PADS_BANK0,
+    let sio = Sio::new(c.device.SIO);
+    let pins = rp_pico::Pins::new(
+      c.device.IO_BANK0,
+      c.device.PADS_BANK0,
       sio.gpio_bank0,
-      &mut p.RESETS,
+      &mut resets,
     );
+    let mut led = pins.led.into_push_pull_output();
+    led.set_low().unwrap();
 
-    let lights = Lights::init(
-      p.PIO0,
-      &mut p.RESETS,
-      clocks.system_clock.get_freq().0 as f32,
-      pins.gpio2.into_mode(),
-      p.TIMER,
-    );
+    let mut timer = hal::Timer::new(c.device.TIMER, &mut resets);
+    let mut alarm = timer.alarm_0().unwrap();
+    let _ = alarm.schedule(SCAN_TIME_US.microseconds());
+    alarm.enable_interrupt(&mut timer);
 
-    let utils = Utils::new(delay);
-
-    let _uart_tx_pin = pins.gpio0.into_mode::<hal::gpio::FunctionUart>();
-    let _uart_rx_pin = pins.gpio1.into_mode::<hal::gpio::FunctionUart>();
-    let _uart = UartPeripheral::new(p.UART0, &mut p.RESETS)
-      .enable(
-        hal::uart::common_configs::_115200_8_N_1,
-        clocks.peripheral_clock.into(),
-      )
-      .unwrap();
-
-    hprintln!("semihosting enabled").unwrap();
-
-    let stack: Vec<Box<dyn Show>> = vec![Box::new(UniformShow::new(Color::WHITE))];
-
-    Self {
-      lights,
-      utils,
-
-      stack,
-    }
+    (Shared { timer, alarm, led }, Local {}, init::Monotonics())
   }
 
-  fn run(mut self) -> ! {
-    loop {
-      if let Some(show) = self.stack.last_mut() {
-        let state = Show::update(show.as_mut(), &mut self.lights, &mut self.utils);
-        if matches!(state, show::State::Finished) {
-          self.stack.pop();
-        }
-      }
+  #[task(
+        binds = TIMER_IRQ_0,
+        priority = 1,
+        shared = [timer, alarm, led],
+        local = [tog: bool = true],
+    )]
+  fn timer_irq(mut c: timer_irq::Context) {
+    if *c.local.tog {
+      c.shared.led.lock(|l| l.set_high().unwrap());
+    } else {
+      c.shared.led.lock(|l| l.set_low().unwrap());
     }
-  }
-}
+    *c.local.tog = !*c.local.tog;
 
-#[entry]
-fn main() -> ! {
-  let app = App::init();
-  app.run();
+    let timer = c.shared.timer;
+    let alarm = c.shared.alarm;
+    (timer, alarm).lock(|t, a| {
+      a.clear_interrupt(t);
+      let _ = a.schedule(SCAN_TIME_US.microseconds());
+    });
+  }
 }
