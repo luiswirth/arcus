2289dbd6d3f48ed5f5ad0373e9cb25823f1ae24e
Luis Wirth <lwirth2000@gmail.com>
2022-02-03 16:02:24 +0100

    add monotonic timer / enable scheduling

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,21 @@ pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 #[rtic::app(device = rp_pico::hal::pac, peripherals = true)]
 mod app {
   use embedded_hal::digital::v2::OutputPin;
-  use embedded_time::duration::Extensions;
-  use rp_pico::hal::{self, clocks, gpio, Sio};
+  use embedded_time::{duration::Extensions, fixed_point::FixedPoint};
+  use rp_pico::hal::{
+    self,
+    clocks::{self, ClockSource},
+    gpio, Sio,
+  };
+  use systick_monotonic::Systick;
+
+  // A monotonic timer to enable scheduling in RTIC
+  #[monotonic(binds = SysTick, default = true)]
+  type MyMono = Systick<100>; // 100 Hz / 10 ms granularity
 
   const SCAN_TIME_US: u32 = 1_000_000;
 
+  // Resources shared between tasks
   #[shared]
   struct Shared {
     timer: hal::Timer,
@@ -24,6 +34,7 @@ mod app {
     led: gpio::Pin<gpio::pin::bank0::Gpio25, gpio::PushPullOutput>,
   }
 
+  // Local resources to specific tasks (cannot be shared)
   #[local]
   struct Local {}
 
@@ -31,7 +42,7 @@ mod app {
   fn init(c: init::Context) -> (Shared, Local, init::Monotonics) {
     let mut resets = c.device.RESETS;
     let mut watchdog = hal::Watchdog::new(c.device.WATCHDOG);
-    let _clocks = clocks::init_clocks_and_plls(
+    let clocks = clocks::init_clocks_and_plls(
       rp_pico::XOSC_CRYSTAL_FREQ,
       c.device.XOSC,
       c.device.CLOCKS,
@@ -42,6 +53,10 @@ mod app {
     )
     .ok()
     .unwrap();
+
+    let systick = c.core.SYST;
+    let systick_freq = clocks.system_clock.get_freq().integer();
+    let mono = Systick::new(systick, systick_freq);
 
     let sio = Sio::new(c.device.SIO);
     let pins = rp_pico::Pins::new(
@@ -58,9 +73,22 @@ mod app {
     let _ = alarm.schedule(SCAN_TIME_US.microseconds());
     alarm.enable_interrupt(&mut timer);
 
-    (Shared { timer, alarm, led }, Local {}, init::Monotonics())
+    (
+      Shared { timer, alarm, led },
+      Local {},
+      init::Monotonics(mono),
+    )
   }
 
+  // Background task, runs whenever no other tasks are running
+  #[idle]
+  fn idle(_: idle::Context) -> ! {
+    loop {
+      continue;
+    }
+  }
+
+  // Hardware task, bound to a hardware interrupt
   #[task(
         binds = TIMER_IRQ_0,
         priority = 1,
