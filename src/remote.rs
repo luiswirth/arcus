a58aa6cd6b2a6d8d67ea99de7639e0505776f38c
Luis Wirth <lwirth2000@gmail.com>
2022-02-05 00:13:35 +0100

    add remote control model

--- src/remote.rs
+++ src/remote.rs
@@ -4,17 +4,18 @@ use crate::{
   show::{self, Show},
 };
 
-use alloc::{boxed::Box, string::ToString};
+use alloc::boxed::Box;
+use infrared::{self as ir, remotecontrol as irrc};
 use rp_pico::hal::gpio;
 use rtic::Mutex;
 use systick_monotonic::*;
 
 pub type IrReceiverPin = gpio::Pin<gpio::bank0::Gpio3, gpio::Input<gpio::Floating>>;
 pub type IrReceiver = infrared::Receiver<
-  infrared::protocol::Nec,
-  infrared::receiver::Poll,
-  infrared::receiver::PinInput<gpio::Pin<gpio::bank0::Gpio3, gpio::Input<gpio::Floating>>>,
-  //Button<CarMp3>,
+  ir::protocol::Nec,
+  ir::receiver::Poll,
+  ir::receiver::PinInput<gpio::Pin<gpio::bank0::Gpio3, gpio::Input<gpio::Floating>>>,
+  irrc::Button<CarMp3>,
 >;
 
 pub const RECEIVER_FREQ_HZ: u32 = 20_000;
@@ -26,11 +27,12 @@ pub struct RemoteTask {
 }
 impl RemoteTask {
   pub fn init(ir_pin: IrReceiverPin) -> Self {
-    let ir_receiver: IrReceiver = infrared::Receiver::builder()
-      .nec()
+    let ir_receiver: IrReceiver = ir::Receiver::builder()
+      .pin(ir_pin)
       .polled()
       .resolution(RECEIVER_FREQ_HZ)
-      .pin(ir_pin)
+      .nec()
+      .remotecontrol(CarMp3)
       .build();
     remote_task::spawn().unwrap();
 
@@ -48,8 +50,12 @@ pub fn remote_task(mut ctx: remote_task::Context) {
 
   let cmd = this.ir_receiver.poll();
   let string = match cmd {
-    Err(e) => Some(format!("Error: {:?} ", e)),
-    Ok(Some(e)) => Some(format!("{:?} ", e)),
+    Err(e) => Some(format!("IrError: {:?}\n", e)),
+    Ok(Some(ref cmd)) => Some(format!(
+      "IrCmd: {:?} IrAction: {:?}\n",
+      cmd.command(),
+      cmd.action()
+    )),
     Ok(None) => None,
   };
   if let Some(string) = string {
@@ -60,10 +66,6 @@ pub fn remote_task(mut ctx: remote_task::Context) {
   }
 
   if let Ok(Some(_)) = cmd {
-    ctx
-      .shared
-      .uart
-      .lock(|uart| uart.write_full_blocking(this.counter.to_string().as_bytes()));
     ctx
       .shared
       .show
@@ -82,4 +84,42 @@ fn next_show(counter: u32) -> Box<dyn Show + Send> {
     5 => Box::new(show::UniformShow::new(Color::NONE)),
     _ => Box::new(show::UniformShow::new(Color::NONE)),
   }
+}
+
+#[derive(Default, Debug)]
+pub struct CarMp3;
+impl irrc::RemoteControlModel for CarMp3 {
+  const MODEL: &'static str = "Car Mp3";
+
+  const DEVTYPE: irrc::DeviceType = irrc::DeviceType::Generic;
+
+  const PROTOCOL: ir::ProtocolId = ir::ProtocolId::Nec;
+
+  const ADDRESS: u32 = 0;
+
+  type Cmd = ir::protocol::NecCommand;
+
+  const BUTTONS: &'static [(u32, infrared::remotecontrol::Action)] = &[
+    (0xFFA25D, irrc::Action::ChannelListPrev),
+    (0xFF629D, irrc::Action::ChannelList),
+    (0xFFE21D, irrc::Action::ChannelListNext),
+    (0xFF22DD, irrc::Action::Prev),
+    (0xFF02FD, irrc::Action::Next),
+    (0xFFC23D, irrc::Action::Play_Pause),
+    (0xFFE01F, irrc::Action::VolumeDown),
+    (0xFFA857, irrc::Action::VolumeUp),
+    (0xFF906F, irrc::Action::Eq),
+    (0xFF6897, irrc::Action::Zero),
+    //(0xFF9867, irrc::Action::?),
+    //(0xFFB04F, irrc::Action::?),
+    (0xFF30CF, irrc::Action::One),
+    (0xFF18E7, irrc::Action::Two),
+    (0xFF7A85, irrc::Action::Three),
+    (0xFF10EF, irrc::Action::Four),
+    (0xFF38C7, irrc::Action::Five),
+    (0xFF5AA5, irrc::Action::Six),
+    (0xFF42BD, irrc::Action::Seven),
+    (0xFF4AB5, irrc::Action::Eight),
+    (0xFF52AD, irrc::Action::Nine),
+  ];
 }
